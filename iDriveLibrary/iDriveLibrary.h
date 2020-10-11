1c00a881a83715e5df8cdd7252e506c6bc7a3d7d
rounakskiran <rounakskiran@gmail.com>
2020-10-12 00:58:19 +0530

    Initial Commit

---
+++ iDriveLibrary/iDriveLibrary.h
@@ -0,0 +1,18 @@
+//
+//  iDriveLibrary.h
+//  iDriveLibrary
+//
+//  Created by Rounak S Kiran on 12/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iDriveLibrary.
+FOUNDATION_EXPORT double iDriveLibraryVersionNumber;
+
+//! Project version string for iDriveLibrary.
+FOUNDATION_EXPORT const unsigned char iDriveLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iDriveLibrary/PublicHeader.h>
+
+
